f0161f609f0594776417d2c3829935a3ac9b61ce
YoonKeumJae <ykj1512@gmail.com>
2024-04-14 17:21:59 +0900

    Logo image add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
-const Banner = styled.div`
+const ImgContainer = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Img = styled.img`
+  max-width: 100%;
   height: 400px;
-  background-color: var(--color-white);
   margin-bottom: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: var(--color-white);
 `;
 
 const Home = () => {
   return (
     <Wrapper>
-      <Banner />
-      <h1>Welcome to our website!</h1>
+      <ImgContainer>
+        <Img src="/Logo.png" />
+      </ImgContainer>
+      <Title>❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️AING❤️</Title>
     </Wrapper>
   );
 };
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
-const Image = styled.img`
+const ImgContainer = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Img = styled.img`
+  max-width: 100%;
+  height: 400px;
+  margin-bottom: 20px;
 `;
 
 const Info = () => {
   return (
     <Wrapper>
-      <Image src="https://via.placeholder.com/800x400" />
+      <ImgContainer>
+        <Img src="/Logo.png" />
+      </ImgContainer>
+      <Img src="https://via.placeholder.com/800x400" />
     </Wrapper>
   );
 };
